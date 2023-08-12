6b03c31b619e682a5386eb649480f51334efa58a
porchsal <porchsal@yahoo.com>
2023-08-12 15:59:44 -0400

    Final commit

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,9 @@
 class Store < ActiveRecord::Base
-
+    has_many :employees
+    validates :name, presence: true
+    validates :name, length: { minimum: 3 }
+    validates :annual_revenue, numericality: {greater_than_equal_to: 0}
+    
+    
+    
 end
